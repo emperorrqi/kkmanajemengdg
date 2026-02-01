2c1aa0ca56474a9dbc8c042ad59c6e6673a3605f
emperorrqi <rizkikamal1102@gmail.com>
2026-02-01 15:41:46 +0700

    Delete app directory

--- app/test_hast.php
+++
@@ -1,9 +0,0 @@
-<?php
-$plain = "admin123";
-$hash = '$2y$10$y0PwCPJf4uXjW6dkqJdIieDz.XgChB4G5HgM0WsmhOrJ3eEFvS5bC';
-
-if (password_verify($plain, $hash)) {
-    echo "✅ Cocok!";
-} else {
-    echo "❌ Tidak cocok!";
-}
